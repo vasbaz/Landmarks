7eed94689d1e4a233da2b8294fb13d9945cba41e
Danila Vasilchenko-Bazarov <vasilchenko-bazarov@yandex.ru>
2020-12-16 21:59:56 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Danila Vasilchenko-Bazarov on 16.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
